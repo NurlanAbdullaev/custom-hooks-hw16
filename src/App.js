fdc91847209a11a0c53a126ba6101633776c5854
NurlanAbdllaev <nurlan2001002@gmail.com>
2023-05-29 20:31:10 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import Input from "./components/Input";
+import { UseTimer } from "./components/UseTimer";
+import { useCounter } from "./hooks/counter";
+
+function App() {
+  const { count, inc, dec, reset } = useCounter();
+  return (
+    <>
+      <UseTimer />
+      <div>
+        <h1>{count} </h1>
+        <button onClick={() => inc(4)}>increment</button>
+        <button onClick={() => dec(2)}>decrement</button>
+        <button onClick={reset}>reset</button>
+      </div>
+      <Input />
+    </>
+  );
+}
+
+export default App;
